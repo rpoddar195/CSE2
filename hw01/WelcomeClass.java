7dcb79eb232ec16117d83e574e4133745d1e2dfa
rpoddar195 <rip218@lehigh.edu>
2015-08-31 14:14:49 +0000

    Updated hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,27 @@
+//////////////////////////////////////////////
+//Ritika Poddar
+//hw01
+//Welcome Class Java Program
+
+
+//define class
+public class WelcomeClass{
+
+//add main method
+    public static void main(String[] args){
+    
+// print main statement with box around it
+    System.out.println("   -----------");
+    System.out.println("   | Welcome |");
+    System.out.println("   -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("<-R--I--P--2--1--8->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("This is my first computer science class.");
+    System.out.println("I hope I am doing this right!");
+    
+    }
+    
+}
